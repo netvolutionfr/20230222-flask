df4be107334eb032185728ee5cc3900f0bca2e97
stanis <stanis@netvolution.fr>
2023-02-23 11:22:23 +0100

    Ajout d'endpoint pour des personnes générées avec Faker

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 import json
+from faker import Faker
 import requests
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+fake = Faker(['fr_FR'])
 
 @app.route('/')
 def index():
@@ -29,12 +31,12 @@ def show_user_profile(username):
     return 'User %s' % user.get('username')
 
 
-NEWS_API_KEY = 'YOUR_API_KEY'
+NEWS_API_KEY = 'SECRET'
 NEWS_API_URL = 'https://newsapi.org/v2/'
 
 @app.route('/news')
 def get_news():
-    response = requests.get(NEWS_API_URL + 'top-headlines?country=fr&apiKey=' + NEWS_API_KEY)
+    response = requests.get(NEWS_API_URL + 'everything?q=python&from=2023-01-22&sortBy=publishedAt&apiKey=' + NEWS_API_KEY)
     content = json.loads(response.content.decode('utf-8'))
     if response.status_code == 200:
         return jsonify({
@@ -46,3 +48,49 @@ def get_news():
             'status': 'error',
             'message': 'Error fetching news'
         })
+
+METEO_API_KEY = 'SECRET'
+METEO_API_URL = 'https://api.openweathermap.org/data/2.5/'
+
+@app.route('/meteo/<city>')
+def get_meteo(city):
+    response = requests.get(METEO_API_URL + 'weather?q=' + city + '&appid=' + METEO_API_KEY)
+    content = json.loads(response.content.decode('utf-8'))
+    if response.status_code == 200:
+        return jsonify({
+            'status': 'success',
+            'data': content
+        })
+    else:
+        return jsonify({
+            'status': 'error',
+            'code': response.status_code,
+            'message': 'Error fetching meteo'
+        })
+
+
+# Utiliser la librairie Faker pour générer des données aléatoires
+liste_faker_personnes = []
+for i in range(10):
+    liste_faker_personnes.append({
+        'id': i,
+        'name': fake.name(),
+        'address': fake.address(),
+        'email': fake.email(),
+    })
+
+
+@app.route('/personnes')
+def get_personnes():
+    return jsonify({
+        'status': 'success',
+        'data': liste_faker_personnes
+    })
+
+
+@app.route('/personnes/<int:id>')
+def get_personne(id):
+    return jsonify({
+        'status': 'success',
+        'data': liste_faker_personnes[id]
+    })
